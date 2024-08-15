c75171e3ae0ce6a9f56058af4e429c0fb669609b
Naohiro MANAGO <manago.naohiro@gmail.com>
2024-08-15 23:37:12 +0900

    NM added files.

---
+++ index_count.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import os
+import sys
+import re
+
+if(len(sys.argv) != 2):
+    raise ValueError(f'Usage: {os.path.basename(sys.argv[0])} input_file_name')
+elif(not os.path.exists(sys.argv[1])):
+    raise IOError(f'Error, no such file >>> {sys.argv[1]}')
+
+with open(sys.argv[1]) as fp:
+    lines = fp.readlines()
+
+for i,line in enumerate(lines):
+    m = re.search('^(\s+)\S+',line)
+    if(not m):
+        continue
+    index = m.group(1)
+    print(f'{i:8d} {len(index)}')
